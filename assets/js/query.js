a7d35c834e11ed08d1b9e5e215ab39e8df2ce876
MarinMalouin <p5oc@free.fr>
2020-01-08 22:33:33 +0100

    Guns initialized

--- assets/js/query.js
+++ assets/js/query.js
@@ -8,7 +8,7 @@ $(function() {
         }
     }
 
-    //Remplissage des block
+    //Création des block
     var listBlock = [];
     var listNumb = [];
     for (var k = 0; k < 12; k++) {
@@ -16,16 +16,49 @@ $(function() {
         let block;
         do {
             numb = Math.floor(Math.random()*100);
+            if (numb < 10) {
+                numb = '0'+numb;
+            }
             block = 'block'+numb;
         } while (listBlock.includes(block));
         listBlock.push(block);
         listNumb.push(numb);
     }
-    
-    
+    //Intégration des block
     for (k = 0; k < 12; k++) {
         let clone = $('<div class="block" id="'+listBlock[k]+'"></div>');
         clone.appendTo($('#case'+listNumb[k]));
     }
+
+    //Initialisation des positions d'armes
+    var listGuns = [];
+    var gunsNumb = [];
+    for (k = 0; k < 4; k++) {
+        let numb;
+        let gun;
+        do {
+            numb = Math.floor(Math.random()*100);
+            if (numb < 10) {
+                numb = '0'+numb;
+            }
+            gun = 'img'+numb;
+        } while (listNumb.includes(numb));
+        listGuns.push(gun);
+        gunsNumb.push(numb);
+    }
     
+    //Création des armes
+    guns = [];
+    guns.push($('<img class="guns" src="assets/img/sniper.svg">'))
+    guns.push($('<img class="guns" src="assets/img/assault.svg">'))
+    guns.push($('<img class="guns" src="assets/img/ak47.svg">'))
+    guns.push($('<img class="guns" src="assets/img/fusil.svg">'))
+    
+    //Intégration des armes à la map
+    for (k = 0; k < 4; k++) {
+        guns[k].appendTo('#case'+gunsNumb[k]);
+    }
+
+    console.log(listGuns)
+
 })
